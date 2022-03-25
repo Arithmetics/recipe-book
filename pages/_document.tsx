480b8353ae8fc4b96a59e1ca2faa01fe1e8076ab
Brock Tillotson <brock.m.tillotson@gmail.com>
2022-03-25 12:08:20 -0700

    add linting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @next/next/no-title-in-document-head */
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme";
