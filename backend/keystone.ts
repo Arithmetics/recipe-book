76936b42e59e8c328963671395a334f6fd8176c9
Brock Tillotson <brock.m.tillotson@gmail.com>
2022-04-09 13:51:00 -0700

    fooling with url stuff

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -27,7 +27,7 @@ export default withAuth(
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     server: {
       cors: {
-        origin: [frontendUrl],
+        origin: frontendUrl,
         credentials: true,
       },
     },
