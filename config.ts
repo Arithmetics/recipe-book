6f53a043df131402ef86f113d9e570ca7bcc95bd
Arithmetics <brock.m.tillotson@gmail.com>
2023-08-07 16:38:26 -0700

    front end url

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 // Client side (public);
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://api.recipebook.ml/api/graphql`;
+export const prodEndpoint = `https://api.brockandrosacook.com/api/graphql`;
